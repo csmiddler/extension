67d241ef856342ee4bc5b06fc9ed0aaecdabedcb
Hank <hank@mullvad.net>
2024-04-23 12:54:22 +0200

    Add svelte instead of React

---
+++ src/popup.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import './lib/styles/index.css';
+</script>
+
+<header>
+  <h1>CSMiddler</h1>
+</header>
+<p>Settings</p>
+
+<style>
+  header {
+    padding: 1rem;
+    background-color:  rgb(27 27 27);
+  }
+
+  p {
+    padding: 0 1rem;
+  }
+</style>
